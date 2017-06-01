2286b7c775970683b84087578d77f55d8568aaaf
ivan <ivan.migov@gmail.com>
2017-06-01 22:07:05 +0300

    midelware

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
-var app = express();
+const app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
 
 app.set('view engine','hbs');
-app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next)=>{
+
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log',log+'\n',(err)=>{
+    err && console.log(err);
+  });
+  next();
+});
+
+// app.use((req, res, next)=>{
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear',()=>{
   return new  Date().getFullYear() + 'now';
 });
